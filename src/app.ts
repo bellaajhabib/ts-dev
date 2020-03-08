91b9777853a727b6ad48a1e197d091afa14e135e
=bellaajhabib <=bellaaj.habib@gmail.com>
2020-03-08 12:11:06 +0100

    tsconfig strict

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ console.log("ssssssssscccc");
 let  a = 5;
 const button = document.querySelector('button')!;
 if(button){
+     let userName = "habib";
     button.addEventListener('click',()=>{
         console.log(new Map());
     });
